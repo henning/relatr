2fd38cbc0e89821a5e5ad6595d3ffdea9a7b5e6a
Henning Sprang <henning@sprang.de>
2009-03-22 20:04:01 +0100

    init

---
+++ grails-app/domain/Contact.groovy
@@ -0,0 +1,17 @@
+class Contact {
+
+    static constraints = {
+    }
+
+    String firstName
+    String lastName
+    String company
+    String phone
+    String mobilePhone
+    String webSite
+    String xingPage
+    //TODO: Tags
+    //String tags
+    String note
+
+}
